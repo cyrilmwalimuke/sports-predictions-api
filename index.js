45a73a47e35841a271f0e4fd424cda8299f3ae07
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2026-04-15 22:32:00 +0300

    random commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const quotes = [
+  "Success is not final, failure is not fatal.",
+  "Do what you can with what you have.",
+  "Stay hungry, stay foolish.",
+];
+
+// Route
+app.get("/quote", (req, res) => {
+  const random = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json({ quote: random });
+});
+
+// Root
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
